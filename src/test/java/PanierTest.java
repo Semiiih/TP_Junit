b6a66bf84aedfc6addaf5b4c85653d88871b4d20
Semih Cakici <semih.cakici34@gmail.com>
2026-03-16 11:28:51 +0100

    test etape 1.2 -> etape 1.5

--- src/test/java/PanierTest.java
+++ src/test/java/PanierTest.java
@@ -14,4 +14,35 @@ class PanierTest {
         assertEquals(1, panier.nombreArticles());
     }
 
+    @Test
+    void calculerTotalDoitRetournerLaSommeDessousTotaux() {
+        // Arranger
+        Panier panier = new Panier();
+        Article article1 = new Article("REF-001", "Stylo bleu", 1.50);
+        Article article2 = new Article("REF-002", "Cahier", 1.50);
+        // Agir
+        panier.ajouterArticle(article1, 3);
+        panier.ajouterArticle(article2, 3);
+        // Affirmer — 2 articles à 1.50 € × 3 = 9.00 €
+        assertEquals(9.00, panier.calculerTotal(), 0.001);
+    }
+
+    @Test
+    void panierVideDoitRetournerEstVideEgalTrue() {
+        // Arranger
+        Panier panier = new Panier();
+        // Affirmer — panier est vide
+        assertTrue(panier.estVide());
+    }
+
+    @Test
+    void panierAvecArticlesNeDoitPasEtreVide() {
+        // Arranger
+        Panier panier = new Panier();
+        Article article1 = new Article("REF-001", "Stylo bleu", 1.50);
+        // Agir
+        panier.ajouterArticle(article1, 3);
+        // Affirmer — panier est vide
+        assertFalse(panier.estVide());
+    }
 }
