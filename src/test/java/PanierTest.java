78409ede25735d6aab54e91b035f132e75816dfa
Semih Cakici <semih.cakici34@gmail.com>
2026-03-16 13:53:43 +0100

    cas limite etape 4 -> etape 4.5

--- src/test/java/PanierTest.java
+++ src/test/java/PanierTest.java
@@ -83,4 +83,49 @@ class PanierTest {
         assertThrows(IllegalArgumentException.class,
                 () -> panier.appliquerCodeReduction(null));
     }
+
+    // Cas limites
+    @Test
+    void quantiteUneDoitEtreAcceptee() {
+        Panier panier = new Panier();
+        Article article = new Article("REF-001", "Stylo", 9.99);
+        panier.ajouterArticle(article, 1);
+        assertEquals(9.99, panier.calculerTotal(), 0.001);
+    }
+
+    @Test
+    void articleGratuitDoitEtreAccepte() {
+        Panier panier = new Panier();
+        Article articleGratuit = new Article("OFFERT-01", "Stylo offert", 0.0);
+        panier.ajouterArticle(articleGratuit, 1);
+        assertEquals(0.0, panier.calculerTotal(), 0.001);
+    }
+
+    @Test
+    void prixEleveDoitFonctionner() {
+        Panier panier = new Panier();
+        Article article = new Article("REF-999", "Article premium", 999.99);
+        panier.ajouterArticle(article, 1);
+        assertEquals(999.99, panier.calculerTotal(), 0.001);
+    }
+
+    @Test
+    void panierAvecUnSeulArticleDoitFonctionner() {
+        Panier panier = new Panier();
+        Article article = new Article("REF-001", "Stylo", 1.50);
+        panier.ajouterArticle(article, 1);
+        assertEquals(1, panier.nombreArticles());
+    }
+
+    @Test
+    void plusieursArticlesDifferentsDansPanier() {
+        Panier panier = new Panier();
+        Article article1 = new Article("REF-001", "Stylo", 1.00);
+        Article article2 = new Article("REF-002", "Cahier", 2.00);
+        Article article3 = new Article("REF-003", "Règle", 3.00);
+        panier.ajouterArticle(article1, 1);
+        panier.ajouterArticle(article2, 1);
+        panier.ajouterArticle(article3, 1);
+        assertEquals(6.00, panier.calculerTotal(), 0.001);
+    }
 }
