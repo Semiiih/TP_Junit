c5b457d2b8fbcac2c3accc9cdd1e294752bb41ed
Semih Cakici <semih.cakici34@gmail.com>
2026-03-16 14:53:21 +0100

    suite de tests  etape 7 -> etape 7.3

---
+++ src/test/java/SuiteBoutique.java
@@ -0,0 +1,13 @@
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({
+        ArticleTest.class,
+        PanierTest.class,
+        PanierReductionTest.class,
+        ServiceCommandeTest.class
+})
+class SuiteBoutique {
+    // Pas de méthode — JUnit exécute les classes déclarées
+}
